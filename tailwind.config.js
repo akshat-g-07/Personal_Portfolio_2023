1a3d040cc2ef234e44507dd3a83f8d90216a3c47
Akshat Garg <akshatg805@gmail.com>
2023-12-15 21:17:38 +0530

    11 tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,21 @@ export default {
         firstDescription: ["Electrolize"],
         secondDescription: ["Poppins"],
       },
+      animation: {
+        helloWordsAnimation:
+          "helloWordsKeyframes 2.5s steps(5,end) alternate infinite,borderKeyframes 0.5s alternate infinite",
+      },
+      keyframes: {
+        helloWordsKeyframes: {
+          "0%": { width: "0px" },
+          "80%": { width: "150px" },
+          "100%": { width: "150px" },
+        },
+        borderKeyframes: {
+          "0%": { borderColor: "white" },
+          "100%": { borderColor: "transparent" },
+        },
+      },
     },
   },
   plugins: [],
