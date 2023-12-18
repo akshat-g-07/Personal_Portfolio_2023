306665e9a94a08e3f2e7693e9b94ba5660dbff1b
Akshat Garg <akshatg805@gmail.com>
2023-12-18 23:40:32 +0530

    18 ResumeButton.jsx

---
+++ src/sections/landingSection/components/ResumeButton.jsx
@@ -0,0 +1,23 @@
+import Button from "@mui/material/Button";
+import LaunchIcon from "@mui/icons-material/Launch";
+
+const ResumeButton = () => {
+  return (
+    <Button
+      color="secondary"
+      variant="contained"
+      endIcon={<LaunchIcon />}
+      className="h-10"
+    >
+      <a
+        href="/resume/Akshat_Garg_Resume_Full_Stack.pdf"
+        download={true}
+        target="_blank"
+      >
+        Resume
+      </a>
+    </Button>
+  );
+};
+
+export default ResumeButton;
