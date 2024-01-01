e33280af2abf29e2a979ce00fce8d670b5ca058e
Akshat Garg <akshatg805@gmail.com>
2024-01-01 21:25:43 +0530

    22 TechWrite.jsx

--- src/sections/landingSection/components/TechWrite.jsx
+++ src/sections/landingSection/components/TechWrite.jsx
@@ -2,7 +2,7 @@ import LaunchIcon from "@mui/icons-material/Launch";
 
 const TechWrite = () => {
   return (
-    <div className="bg-[url('/images/techImage.png')] w-32 h-32 bg-no-repeat bg-cover flex items-center justify-center animate-spinSlow mt-6 cursor-pointer">
+    <div className="bg-[url('/images/techImage.png')] w-32 h-32 bg-no-repeat bg-contain bg-center flex items-center justify-center animate-spinSlow mt-6 cursor-pointer">
       <div className="animate-spinRev">
         <LaunchIcon className="text-[#9c27b0] hover:text-[#7b1fa2]" />
       </div>
