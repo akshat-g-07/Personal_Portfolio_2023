34393aaa23788a4ad808e567d0c41b9749f97331
Akshat Garg <akshatg805@gmail.com>
2024-01-03 21:32:40 +0530

    23 SkillSection.jsx

---
+++ src/sections/skillSection/SkillSection.jsx
@@ -0,0 +1,11 @@
+const SkillSection = () => {
+  return (
+    <div className="w-screen h-auto absolute top-16 flex flex-col select-none pl-10 pr-3 lg:pr-40 md:px-14">
+      <div className="text-left w-max font-sectionHeading text-2xl md:text-5xl text-white">
+        My {window.innerWidth < 768 && <br />} Skills
+      </div>
+    </div>
+  );
+};
+
+export default SkillSection;
