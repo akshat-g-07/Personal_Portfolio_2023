50cb7374cbee51116b6da4cf9abcd0419532faea
Akshat Garg <akshatg805@gmail.com>
2024-01-06 23:03:17 +0530

    28 tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
           "helloWordsKeyframes 2.5s steps(5,end) alternate infinite,borderKeyframes 0.5s alternate infinite",
         spinSlow: "spin 15s linear infinite",
         spinRev: "spin 15s linear infinite reverse",
+        projectSlideAnimation: "projectSlideKeyframes 15s infinite linear",
       },
       keyframes: {
         helloWordsKeyframes: {
@@ -23,6 +24,41 @@ export default {
         borderKeyframes: {
           "0%": { borderColor: "white" },
           "100%": { borderColor: "transparent" },
+        },
+        projectSlideKeyframes: {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "10%": {
+            transform: "translateX(0)",
+          },
+          "20%": {
+            transform: "translateX(-20%)",
+          },
+          "30%": {
+            transform: "translateX(-20%)",
+          },
+          "40%": {
+            transform: "translateX(-40%)",
+          },
+          "50%": {
+            transform: "translateX(-40%)",
+          },
+          "60%": {
+            transform: "translateX(-60%)",
+          },
+          "70%": {
+            transform: "translateX(-60%)",
+          },
+          "80%": {
+            transform: "translateX(-80%)",
+          },
+          "90%": {
+            transform: "translateX(-80%)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
         },
       },
     },
