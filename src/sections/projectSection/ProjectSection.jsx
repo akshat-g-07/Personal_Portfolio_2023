ac8249b2c9fa5f37d8d3b807f84ef34a06fd1638
Akshat Garg <akshatg805@gmail.com>
2024-01-11 19:39:45 +0530

    30 ProjectSection.jsx

--- src/sections/projectSection/ProjectSection.jsx
+++ src/sections/projectSection/ProjectSection.jsx
@@ -1,9 +1,11 @@
+import ProjectBody from "./components/ProjectBody";
 const ProjectSection = () => {
   return (
     <div className="w-screen h-auto absolute top-16 flex flex-col select-none pl-10 pr-3 lg:pr-40 md:px-14">
       <div className="text-left w-max font-sectionHeading text-2xl md:text-5xl text-white">
         Some things {window.innerWidth < 768 && <br />} I've built
       </div>
+      <ProjectBody />
     </div>
   );
 };
