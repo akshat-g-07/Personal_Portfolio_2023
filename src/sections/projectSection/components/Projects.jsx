3f3a7e36a381e2a302d11ae38595494df166742f
Akshat Garg <akshatg805@gmail.com>
2024-01-18 19:15:18 +0530

    37 Projects.jsx

---
+++ src/sections/projectSection/components/Projects.jsx
@@ -0,0 +1,59 @@
+const Projects = [
+  {
+    name: "TrekLicious",
+    github: "https://github.com/akshat-g-07/TrekLicious-frontend",
+    live: "https://treklicious.onrender.com/",
+    skills: ["NodeJS", "MongoDB", "ExpressJS", "ReactJS", "API", "MaterialUI"],
+    images: [
+      "land01.png",
+      "signup.png",
+      "land03.png",
+      "login.png",
+      "land02.png",
+    ],
+    description: [
+      "Developed a preference-based website with a user-friendly interface that specifically caters to trek lovers.",
+      "The website’s main objective is to assist trek lovers in determining the appropriate treks for themselves.",
+      "User can signup/login, update their details to see treks based on their preferences.",
+      "User can also create a favorite list and add/remove items from their list.",
+    ],
+  },
+  {
+    name: "RatVenture",
+    github: "",
+    live: "https://rat-frontend.onrender.com/",
+    skills: ["ReactJS", "ExpressJS", "Javascript", "API", "MaterialUI"],
+    images: [
+      "land01.png",
+      "signup.png",
+      "land03.png",
+      "login.png",
+      "land02.png",
+    ],
+    description: [
+      "Visualization of famous Rat-In-A-Maze problem.",
+      "User can dynamically give the size of the matrix, starting point, blocks and ending point.",
+      "User can also choose to give the random values to above required values.",
+      "User will be able to see all the possible ways from starting point to ending point.",
+    ],
+  },
+  {
+    name: "GrillZilla",
+    github: "https://github.com/akshat-g-07/GrillZilla",
+    live: "https://restaurant-bulc.onrender.com/",
+    skills: ["ReactJS", "API", "Axios"],
+    images: [
+      "land01.png",
+      "signup.png",
+      "land03.png",
+      "login.png",
+      "land02.png",
+    ],
+    description: [
+      "A frontend application to show the menu of the restaurant.",
+      "Application also uses Axios API to fetch user’s location and show him the restaurants closest to him.",
+      "Using Google Maps API user can see the directions to the restaurant location from his/her.",
+    ],
+  },
+];
+export default Projects;
