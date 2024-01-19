df4bb9e5cee838959bf68c5abc6fdb60cd8fa259
Akshat Garg <akshatg805@gmail.com>
2024-01-19 21:07:12 +0530

    40 ImagesPart.jsx

--- src/sections/projectSection/components/ImagesPart.jsx
+++ src/sections/projectSection/components/ImagesPart.jsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 
 const ImagesPart = ({ images, name }) => {
+  const divWidth = window.innerWidth < 768 ? "18rem" : "20rem";
   useEffect(() => {
     const copy = document
       .querySelector(".projectImageCarouselChild")
@@ -14,7 +15,7 @@ const ImagesPart = ({ images, name }) => {
       <div
         className="projectImageCarouselChild flex animate-projectSlideAnimation"
         style={{
-          width: `calc(18rem * ${images.length})`,
+          width: `calc(${divWidth} * ${images.length})`,
         }}
       >
         {images.map((image) => {
@@ -24,7 +25,7 @@ const ImagesPart = ({ images, name }) => {
               style={{
                 backgroundImage: `url('./projects/${name.toLowerCase()}/${image}')`,
               }}
-              className="h-40 w-72 bg-center bg-no-repeat bg-cover"
+              className="h-40 w-72 md:w-80 bg-center bg-no-repeat bg-cover"
             />
           );
         })}
