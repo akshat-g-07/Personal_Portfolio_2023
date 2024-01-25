aa0a42ec25f873073a3229ad65990569493a4b18
Akshat Garg <akshatg805@gmail.com>
2024-01-25 23:47:09 +0530

    Update HelloWords.jsx

--- src/sections/landingSection/components/HelloWords.jsx
+++ src/sections/landingSection/components/HelloWords.jsx
@@ -9,7 +9,7 @@ const HelloWords = () => {
     setWordIndex(tempIndex);
   }, 1000 * 5);
 
-  const helloWords = ["नमस्ते", "Hello", "Hallo", "Hola", "Ciao", "Helló"];
+  const helloWords = ["नमस्ते", "Helloo", "Hallo", "Hola", "Ciao", "Helló"];
 
   return (
     <div className="w-24 md:w-44 text-white border-r-8 text-2xl md:text-5xl font-helloWordsFont overflow-y-clip overflow-x-hidden animate-helloWordsAnimation origin-left">
