a2bcf2f09c7c63a460e427d5e18496a03695a692
Akshat Garg <akshatg805@gmail.com>
2024-01-28 18:09:00 +0530

    Update ContactSection.jsx

--- src/sections/contactSection/ContactSection.jsx
+++ src/sections/contactSection/ContactSection.jsx
@@ -71,7 +71,7 @@ const ContactSection = () => {
       >
         <div className="text-left w-max font-sectionHeading text-2xl md:text-5xl text-white">
           Let's get {window.innerWidth < 768 && <br />} in TOUCH!
-          <div className="font-sectionDescription flex items-center text-sm md:text-2xl lg:text-3xl my-1 lg:my-5">
+          <div className="font-sectionDescription flex flex-row items-center text-sm md:text-2xl lg:text-3xl my-1 lg:my-5">
             <div className="w-[20px] h-0.5 mr-2 bg-white" />
             Contact Me
           </div>
