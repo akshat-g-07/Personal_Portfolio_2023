09072d9a12a98bd42f457e2cd7f7c1a21a45034d
Akshat Garg <akshatg805@gmail.com>
2024-02-09 21:33:40 +0530

    Update SkillSection.jsx

--- src/sections/skillSection/SkillSection.jsx
+++ src/sections/skillSection/SkillSection.jsx
@@ -2,7 +2,7 @@ import SkillBody from "./components/SkillBody";
 
 const SkillSection = () => {
   return (
-    <div className="w-screen h-auto absolute top-16 flex flex-col select-none pl-10 pr-3 lg:pr-40 md:px-14">
+    <div className="w-screen h-auto absolute top-16 flex  select-none pl-10 pr-3 lg:pr-40 md:px-14">
       <div className="text-left w-max font-sectionHeading text-2xl md:text-5xl text-white">
         One or two {window.innerWidth < 768 && <br />} things I KNOW!
         <div className="font-sectionDescription flex items-center text-sm md:text-2xl lg:text-3xl my-1 lg:my-5">
