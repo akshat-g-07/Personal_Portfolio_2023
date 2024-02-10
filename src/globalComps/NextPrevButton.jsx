18b33f9cdedaa0f487ebde6276f540ca9dcf3603
Akshat Garg <akshatg805@gmail.com>
2024-02-10 22:01:46 +0530

    61 NextPrevButton.jsx

--- src/globalComps/NextPrevButton.jsx
+++ src/globalComps/NextPrevButton.jsx
@@ -1,56 +1,35 @@
+/* eslint-disable react/prop-types */
 import { motion } from "framer-motion";
 import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
 
-const NextPrevButton = ({
-  // eslint-disable-next-line react/prop-types
-  scrollTo,
-  // eslint-disable-next-line react/prop-types
-  setScrollTo,
-  // eslint-disable-next-line react/prop-types
-  setScrollDown,
-  // eslint-disable-next-line react/prop-types
-  setTransitionTime,
-}) => {
-  const sectionArray = [
-    "landing",
-    "skills",
-    "projects",
-    "experience",
-    "education",
-    "hobby",
-    "contact",
-  ];
-  const timeStamps = [0, 1.5, 2.5, 4.25, 5.5, 6.5, 8.5];
+const NextPrevButton = ({ sectionArray, setSectionIndex, sectionIndex }) => {
   return (
     <>
       <motion.div
-        className="fixed flex flex-col justify-between h-5/6 lg:h-full right-1 md:right-10 lg:right-16 z-20 text-stone-100 select-none text-lg py-8"
+        className="fixed flex flex-col justify-between items-center h-full right-1 md:right-10 lg:right-16 z-20 text-stone-100 select-none text-lg py-8"
         animate={{ opacity: [0, 1], y: [100, 0] }}
         transition={{ type: "spring", delay: 2.5 }}
       >
         <motion.div
           className={
-            scrollTo === "landing"
+            sectionArray[sectionIndex].key === "landing"
               ? "flex items-center cursor-not-allowed scale-100 text-stone-500"
               : "flex items-center cursor-pointer scale-100 hover:font-semibold"
           }
           whileHover={
-            scrollTo !== "landing" && {
+            sectionArray[sectionIndex].key !== "landing" && {
               scale: 1.15,
               transition: { type: "spring" },
             }
           }
-          whileTap={scrollTo !== "landing" && { scale: 0.8 }}
+          whileTap={
+            sectionArray[sectionIndex].key !== "landing" && { scale: 0.8 }
+          }
           onClick={() => {
-            let indexVal = sectionArray.indexOf(scrollTo);
-            if (indexVal === 0) return;
-            setTransitionTime(timeStamps[indexVal] - timeStamps[indexVal - 1]);
-            indexVal -= 1;
-            setScrollTo("transition");
-            setScrollDown(-1);
-            setTimeout(() => {
-              setScrollTo(sectionArray[indexVal]);
-            }, 5);
+            if (sectionIndex === 0) return;
+            let indexVal = sectionIndex - 1;
+            setSectionIndex(indexVal);
+            window.playAnimation(-1, sectionArray[indexVal].key);
           }}
         >
           <motion.div
@@ -62,28 +41,26 @@ const NextPrevButton = ({
           &nbsp;Previous
         </motion.div>
         <motion.div
+          id="nextAnimButton"
           className={
-            scrollTo === "contact"
+            sectionArray[sectionIndex].key === "contact"
               ? "flex items-center cursor-not-allowed scale-100 text-stone-500"
               : "flex items-center cursor-pointer scale-100 hover:font-semibold"
           }
           whileHover={
-            scrollTo !== "contact" && {
+            sectionArray[sectionIndex].key !== "contact" && {
               scale: 1.15,
               transition: { type: "spring" },
             }
           }
-          whileTap={scrollTo !== "contact" && { scale: 0.8 }}
+          whileTap={
+            sectionArray[sectionIndex].key !== "contact" && { scale: 0.8 }
+          }
           onClick={() => {
-            let indexVal = sectionArray.indexOf(scrollTo);
-            if (indexVal === 6) return;
-            indexVal += 1;
-            setTransitionTime(timeStamps[indexVal] - timeStamps[indexVal - 1]);
-            setScrollTo("transition");
-            setScrollDown(1);
-            setTimeout(() => {
-              setScrollTo(sectionArray[indexVal]);
-            }, 5);
+            if (sectionIndex === 6) return;
+            let indexVal = sectionIndex + 1;
+            setSectionIndex(indexVal);
+            window.playAnimation(1, sectionArray[indexVal].key);
           }}
         >
           Next&nbsp;
