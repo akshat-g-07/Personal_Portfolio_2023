ca22e531061f59585935993c05cf7e0d94db14ba
Akshat Garg <akshatg805@gmail.com>
2024-02-12 23:00:38 +0530

    66 EducationSection.jsx

--- src/sections/educationSection/EducationSection.jsx
+++ src/sections/educationSection/EducationSection.jsx
@@ -1,17 +1,32 @@
 import { Avatar } from "@mui/material";
+import IncomingAnimation from "../../globalComps/IncomingAnimation";
+import { motion } from "framer-motion";
+
 const EducationSection = () => {
   return (
-    <div className="w-screen lg:w-max h-auto absolute top-72 lg:top-24 lg:right-48 lg:px-0 flex flex-col select-none pl-10 pr-3 md:px-14">
-      <div className="text-left w-max font-sectionHeading text-xl md:text-4xl lg:text-5xl text-white">
+    <div className="w-screen lg:w-max h-auto absolute top-80 lg:top-24 lg:right-48 lg:px-0 flex flex-col select-none pl-10 pr-3 md:px-14">
+      <motion.div
+        className="text-left w-max font-sectionHeading text-xl md:text-4xl lg:text-5xl text-white"
+        animate={{
+          ...IncomingAnimation,
+          transition: { ...IncomingAnimation.transition, delay: 0.5 },
+        }}
+      >
         Institution that {window.innerWidth < 768 && <br />} shaped ME.
         <div className="font-sectionDescription flex items-center text-sm md:text-2xl lg:text-3xl my-1 lg:my-5">
           <div className="w-[20px] h-0.5 mr-2 bg-white" />
           Education
         </div>
-      </div>
-      <div className="text-white font-firstDescription font-bold text-xl md:text-3xl pt-4 flex items-center">
+      </motion.div>
+      <motion.div
+        className="text-white font-firstDescription font-bold text-xl md:text-3xl pt-4 flex items-center"
+        animate={{
+          ...IncomingAnimation,
+          transition: { ...IncomingAnimation.transition, delay: 1 },
+        }}
+      >
         <Avatar
-          alt="NIT"
+          alt="N I T"
           src="/images/nitp.png"
           sx={{ height: 56, width: 56 }}
         />
@@ -20,11 +35,11 @@ const EducationSection = () => {
           <span className="text-stone-400 text-sm md:text-lg font-bold font-secondDescription block md:inline">
             Patna
           </span>
-          <span className="text-stone-600 text-xs md:text-base font-secondDescription block md:my-2">
+          <span className="text-stone-500 text-xs md:text-base font-secondDescription block md:my-2">
             2017-2021
           </span>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
