b39f19861e6f4a9ed83f54d2fa58784a5e7061fc
Akshat Garg <akshatg805@gmail.com>
2024-02-15 23:35:23 +0530

    72 HelloWords.jsx

--- src/sections/landingSection/components/HelloWords.jsx
+++ src/sections/landingSection/components/HelloWords.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion } from "framer-motion";
 
 const HelloWords = () => {
   const [wordIndex, setWordIndex] = useState(0);
@@ -7,14 +8,40 @@ const HelloWords = () => {
     let tempIndex = wordIndex;
     tempIndex = (tempIndex + 1) % helloWords.length;
     setWordIndex(tempIndex);
-  }, 1000 * 5);
+  }, 5000);
 
-  const helloWords = ["नमस्ते", "Helloo", "Hallo", "Hola", "Ciao", "Helló"];
+  const helloWords = [
+    "नमस्ते",
+    "Hello",
+    "Hallo",
+    "Hola",
+    "Ciao",
+    "Helló",
+    "Bonjour",
+    "Olá",
+  ];
 
   return (
-    <div className="w-24 md:w-44 text-white border-r-8 text-2xl md:text-5xl font-helloWordsFont overflow-y-clip overflow-x-hidden animate-helloWordsAnimation origin-left">
-      {helloWords[wordIndex]}
+    <div className="w-24 md:w-52 text-white text-center text-2xl md:text-5xl font-helloWordsFont overflow-hidden">
+      <motion.div
+        className="w-full h-full"
+        animate={{
+          x: ["0%"],
+          y: ["100%", "0%", "0%", "-100%"],
+          scale: [1],
+          transition: {
+            duration: 5,
+            times: [0, 0.25, 0.75, 1],
+            repeat: Infinity,
+          },
+        }}
+      >
+        {helloWords[wordIndex]}
+      </motion.div>
     </div>
+    // <div className="w-24 pl-3 md:w-44 text-white border-r-8 text-2xl md:text-5xl font-helloWordsFont overflow-y-clip overflow-x-hidden animate-helloWordsAnimation origin-left">
+    //   {helloWords[wordIndex]}
+    // </div>
   );
 };
 
