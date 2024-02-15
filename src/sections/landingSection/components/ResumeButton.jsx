b36a9da72e3355b7e2e81d408d3210049fad9b06
Akshat Garg <akshatg805@gmail.com>
2024-02-15 23:36:27 +0530

    74 ResumeButton.jsx

--- src/sections/landingSection/components/ResumeButton.jsx
+++ src/sections/landingSection/components/ResumeButton.jsx
@@ -9,11 +9,7 @@ const ResumeButton = () => {
       endIcon={<LaunchIcon />}
       className="h-10"
     >
-      <a
-        href="/resume/Akshat_Garg_Resume_Full_Stack.pdf"
-        download={true}
-        target="_blank"
-      >
+      <a href="/resume/Akshat_Garg_Resume.pdf" download={true} target="_blank">
         Resume
       </a>
     </Button>
